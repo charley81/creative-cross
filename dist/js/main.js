fc99590dffa94154b3881f12d17741a76b99fc91
charley81 <chrisharley81@gmail.com>
2019-02-24 10:34:41 -0500

    fixed about section .item layout

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+// Hide Logo On Scroll
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 200) {
+    document.querySelector("#navbar h3").style.opacity = 0;
+    document.querySelector("#navbar .hamburger").style.border =
+      "1px solid #fff";
+  } else {
+    document.querySelector("#navbar h3").style.opacity = 1;
+    document.querySelector("#navbar .hamburger").style.border = "none";
+  }
+});
